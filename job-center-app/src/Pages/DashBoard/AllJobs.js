dd9265391ff3ee6832d5ba0c61595109079ae436
Ozan <ozan.yasin97@gmail.com>
2022-08-03 03:08:07 +0300

    ticket-08 done

--- job-center-app/src/Pages/DashBoard/AllJobs.js
+++ job-center-app/src/Pages/DashBoard/AllJobs.js
@@ -1,7 +1,12 @@
-import React from "react";
+import { JobsContainer, SearchContainer } from '../../components';
 
 const AllJobs = () => {
-  return <h1>AllJobs</h1>;
+  return (
+    <>
+      <SearchContainer />
+      <JobsContainer />
+    </>
+  );
 };
 
 export default AllJobs;
