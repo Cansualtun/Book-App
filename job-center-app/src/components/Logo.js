5ca6b7dec3e59ebad26e0f51ad4783865c8ac7d6
Ozan <ozan.yasin97@gmail.com>
2022-08-13 03:28:46 +0300

    duplicated files deleted

--- job-center-app/src/components/Logo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import logo from "../assets/images/android-chrome-192x192.png";
-
-const Logo = () => {
-  return <img src={logo} alt="job-center logo" className="logo" />;
-};
-
-export default Logo;
